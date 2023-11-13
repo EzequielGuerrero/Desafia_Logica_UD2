26f730d5fc4da2d7d0c8f7788e0a23ae7a2763ae
EzequielGuerrero <ezequiel_guerrero1@hotmail.com>
2023-11-13 23:44:48 +0100

    PrimerCambio

---
+++ EjerciciosProgramacionUnidad2/Cuenta_Numeros_1_10.java
@@ -0,0 +1,6 @@
+public class Cuenta_Numeros_1_10 {
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
--- EjerciciosProgramacionUnidad2/Numeros_1_10.java
+++
@@ -1,7 +0,0 @@
-package EjerciciosProgramacionUnidad2;
-
-public class Numeros_1_10 {
-    public static void main(String[] args) {
-
-    }
-}
