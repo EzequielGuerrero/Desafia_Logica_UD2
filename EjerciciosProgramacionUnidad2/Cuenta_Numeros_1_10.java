75b1f29361742ac779fca0e264f5f9ae5ec67225
EzequielGuerrero <ezequiel_guerrero1@hotmail.com>
2023-11-13 23:49:56 +0100

    PrimerEjercicioCompleto

--- EjerciciosProgramacionUnidad2/Cuenta_Numeros_1_10.java
+++ EjerciciosProgramacionUnidad2/Cuenta_Numeros_1_10.java
@@ -1,6 +1,20 @@
 public class Cuenta_Numeros_1_10 {
 
     public static void main(String[] args) {
-        System.out.println("hola");
+
+        // Escribe un programa en Java que muestre los números del 10 al 1, en ese
+        // orden. Debes utilizar una estructura de tipo while ó do-while.
+
+        int contador = 10;
+
+        while (contador >= 1) {
+
+            System.out.println(contador);
+
+            contador--;
+
+        }
+
     }
+
 }
